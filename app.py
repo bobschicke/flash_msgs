80516dc81170abbc135ada8fc3411d049f7e6b8f
bobschicke <rws7696@njit.edu>
2021-12-13 23:53:28 -0500

    Made some style changes.

--- app.py
+++ app.py
@@ -10,8 +10,7 @@ app.wsgi_app = DebuggedApplication(app.wsgi_app, True)
 
 @app.route('/')
 def flash_test():  # put application's code here
-    flash("Welcome to my Flash Test!")
-
+    flash("Welcome to my Flash Message Demo!............Click on the X to close the message.")
     return render_template("flash_msgs.html")
 
 
